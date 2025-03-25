3860fc841f0e194e8c5f23d18513fc33198dd113
thomaspeace <thomasjpeace29@gmail.com>
2025-03-25 23:10:27 +0000

    replace part1 with part2 refactored code

--- day02/python/part1.py
+++ day02/python/part1.py
@@ -6,11 +6,10 @@ with open("input.txt", "r") as file:
         level = []
         level.append(line.strip())
         levels += level
-        
-for level in levels:
-    level = level.split()
 
-    # check if the first 2 element are decending
+
+def check_level_safety(level):
+
     ascending = False
     if int(level[0]) < int(level[1]):
         ascending = True
@@ -26,10 +25,12 @@ for level in levels:
             if (int(level[i]) - int(level[i + 1])) > 3 or (int(level[i]) - int(level[i + 1])) <= 0:
                 is_safe = False
 
-    if is_safe:
+    return is_safe
+    
+
+for level in levels:
+    level = level.split()
+    if check_level_safety(level):
         safe_count += 1
 
 print(safe_count)
-
-
-
