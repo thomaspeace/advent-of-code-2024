305d463d80aa6c862c6d91fed121d3e8f7b33a49
thomaspeace <thomasjpeace29@gmail.com>
2025-03-30 13:39:00 +0100

    add printing of possible mul string

--- day03/python/part1.py
+++ day03/python/part1.py
@@ -16,4 +16,12 @@ mul_idx = []
 for line in lines:
   mul_idx.append(find_valid_substring_indexes(line, "mul("))
 
-print(mul_idx)
+
+# a number is valid if it's less than 1-3 digits, and in between mul(xxx,yyy)
+def find_valid_num(string):
+  print(string)
+   
+   
+for i in range(len(lines)):
+  for idx in mul_idx[i]:
+    find_valid_num(lines[i][idx:idx+12])
